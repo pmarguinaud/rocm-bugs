800c8601b7d8a516d69a7b9a5267c5a4d77e378d
Philippe Marguinaud <philippe.marguinaud@meteo.fr>
2026-01-27 13:34:39 +0100

    Compile without GPU options

--- mysort1/run.sh
+++ mysort1/run.sh
@@ -3,4 +3,11 @@ set -x
 
 export PATH=/ec/res4/hpcperm/sor/install/rocm/8873/bin:$PATH
 
+amdclang mysort.c 
+
+\ls -l a.out
+
 amdclang -fopenmp --offload-arch=gfx942 -lflang_rt.hostdevice mysort.c 
+
+\ls -l a.out
+

9e3a964ab79ea4eda115bcaf426b9abcee07a55f
Philippe Marguinaud <philippe.marguinaud@meteo.fr>
2026-01-27 09:38:40 +0000

    Prune

--- mysort/run.sh
+++ mysort/run.sh
@@ -3,5 +3,5 @@ set -x
 
 export PATH=/ec/res4/hpcperm/sor/install/rocm/8873/bin:$PATH
 
-amdclang -fopenmp --offload-arch=gfx942 -lflang_rt.hostdevice -O1 -g -c mysort.c
-amdclang -fopenmp --offload-arch=gfx942 -lflang_rt.hostdevice -O1 -g -fopenmp -Wl,-z,execstack --offload-arch=gfx942 -lflang_rt.hostdevice mysort.o -o mysort 
+amdclang -fopenmp --offload-arch=gfx942 -lflang_rt.hostdevice -c mysort.c
+amdclang -fopenmp --offload-arch=gfx942 -lflang_rt.hostdevice -fopenmp --offload-arch=gfx942 -lflang_rt.hostdevice mysort.o -o mysort 

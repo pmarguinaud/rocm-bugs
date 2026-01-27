ae41bab816775322dc3ea22533d9c6d330e52f73
Philippe Marguinaud <philippe.marguinaud@meteo.fr>
2026-01-27 12:17:27 +0000

    Prune

--- mysort1/mysort.c
+++ mysort1/mysort.c
@@ -4,39 +4,30 @@
 #include <unistd.h>
 #include <ctype.h>
 
-#undef MAX
-#define MAX(a,b) ( ((a) > (b)) ? (a) :  (b) )
-
-#define ALLOC(x,size) { \
-  long long int _nbytes = MAX(1,(size)) * sizeof(*(x)); \
-  x = malloc(_nbytes); \
-  if (!x) { \
-    fprintf(stderr,"***Error: Unable to ALLOCate %lld bytes at %s:%d\n",\
-            _nbytes, __FILE__, __LINE__); \
-    abort(); \
-  } \
-}
-
-int
-main(int argc, char *argv[])
+int main (int argc, char *argv[])
 {
 
   int j, jn;
   char **new_argv = NULL;
-  
-  for (j=0; j<argc; ) {
-    char *opt = argv[j];
-    int pos1 = atoi(opt+1)+1;
-    int pos2 = pos1;
 
-    int len = 100;
-    ALLOC(new_argv[jn], len);
-    snprintf(new_argv[jn++], len, "-k%d,%d",pos1,pos2);
-
-    ++j;
-  }
+  for (j = 0; j < argc; j++) 
+    {
+      char *opt = argv[j];
+      int pos1 = atoi(opt+1)+1;
+      int pos2 = pos1;
+   
+      int len = 100;
+      { 
+        long long int _nbytes = ( ((1) > ((len))) ? (1) : ((len)) ) * sizeof(*(new_argv[jn])); 
+        new_argv[jn] = malloc (_nbytes); 
+        if (! new_argv[jn]) 
+          { 
+            fprintf(stderr,"***Error: Unable to ALLOCate %lld bytes at %s:%d\n", _nbytes, "toto.c", 27); 
+            abort(); 
+          } 
+      }
+      snprintf(new_argv[jn++], len, "-k%d,%d",pos1,pos2);
+    }
 
   return 0;
-
 }
-

2b6a0eeeb6802a79a53a2a5aad4594db4c853ec3
Philippe Marguinaud <philippe.marguinaud@meteo.fr>
2026-01-27 09:29:48 +0000

    Add mysort

---
+++ mysort/run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -x
+
+export PATH=/ec/res4/hpcperm/sor/install/rocm/8873/bin:$PATH
+
+amdclang  -DLINUX -DLITTLE  -fopenmp -Wl,-z,execstack \
+  --offload-arch=gfx942 -lflang_rt.hostdevice -O1 -g -fPIE \
+  -c mysort.c
+
+amdclang -fopenmp -Wl,-z,execstack --offload-arch=gfx942 -lflang_rt.hostdevice \
+  -O1 -g -Wl,--export-dynamic -fopenmp -Wl,-z,execstack --offload-arch=gfx942 \
+  -lflang_rt.hostdevice
+  mysort.o -o mysort 

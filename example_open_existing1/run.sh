03c5fcf22a1b33c30d5e84292f5e7c1eb6d03b60
Philippe Marguinaud <philippe.marguinaud@meteo.fr>
2026-01-28 20:52:53 +0000

    Add example_open_existing1

--- example_open_existing1/run.sh
+++ example_open_existing1/run.sh
@@ -4,11 +4,13 @@ export PATH=/ec/res4/hpcperm/sor/install/rocm/8873/bin:$PATH
 
 set -x
 
+\rm -f *.o *.mod *.a *.x
+
 FC="amdflang -Wl,--export-dynamic -fopenmp -Wl,-z,execstack --offload-arch=gfx942 -lflang_rt.hostdevice -fPIC -fconvert=big-endian -Qunused-arguments -fopenmp -Wl,-z,execstack --offload-arch=gfx942 -O1 -g"
 
-$FC -c yomvirt1.F90 -o yomvirt1.F90.o
-$FC -c example_open_existing.F90 -o example_open_existing.F90.o
+$FC -c yomvirt1.F90 
+$FC -c example_open_existing.F90 
 \rm -f libodb.a
-ar crv libodb.a yomvirt1.F90.o
-$FC example_open_existing.F90.o -o example_open_existing.x libodb.a libodb.a 
+ar crv libodb.a yomvirt1.o
+$FC example_open_existing.o -o example_open_existing.x libodb.a libodb.a 
 

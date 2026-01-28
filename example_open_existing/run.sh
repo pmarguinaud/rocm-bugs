b45b79c17d906666927cde34c696f92f03727e34
Philippe Marguinaud <philippe.marguinaud@meteo.fr>
2026-01-28 13:37:01 +0000

    Prune

--- example_open_existing/run.sh
+++ example_open_existing/run.sh
@@ -5,4 +5,4 @@ set -x
 export PATH=/ec/res4/hpcperm/sor/install/rocm/8873/bin:$PATH
 
 amdflang -fopenmp --offload-arch=gfx942 -c example_open_existing.F90 
-amdflang -fopenmp --offload-arch=gfx942 -lflang_rt.hostdevice -fPIC -Qunused-arguments -fopenmp --offload-arch=gfx942 -lflang_rt.hostdevice -g example_open_existing.o -o example_open_existing 
+amdflang -fopenmp --offload-arch=gfx942 -lflang_rt.hostdevice -fopenmp --offload-arch=gfx942 -lflang_rt.hostdevice -g example_open_existing.o -o example_open_existing 

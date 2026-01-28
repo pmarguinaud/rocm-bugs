0559b0abdf3efe7ae73394829f6372826f3a2ec7
Philippe Marguinaud <philippe.marguinaud@meteo.fr>
2026-01-28 20:58:42 +0000

    Prune

--- example_open_existing1/run.sh
+++ example_open_existing1/run.sh
@@ -4,7 +4,7 @@ export PATH=/ec/res4/hpcperm/sor/install/rocm/8873/bin:$PATH
 
 set -x
 
-FC="amdflang -Wl,--export-dynamic -fopenmp -Wl,-z,execstack -fPIC -fconvert=big-endian -Qunused-arguments -fopenmp -Wl,-z,execstack -O1 -g"
+FC="amdflang -fopenmp -fPIC -O1 -g"
 
 for opt in "" "--offload-arch=gfx942 -lflang_rt.hostdevice"
 do

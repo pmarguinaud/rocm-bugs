7eaaddb95acc26e2775e2c1e0e43a13a3e3f7501
Philippe Marguinaud <philippe.marguinaud@meteo.fr>
2026-01-28 20:52:17 +0000

    Add example_open_existing1

--- example_open_existing1/run.sh
+++ example_open_existing1/run.sh
@@ -4,13 +4,11 @@ export PATH=/ec/res4/hpcperm/sor/install/rocm/8873/bin:$PATH
 
 set -x
 
-amdflang -Wl,--export-dynamic -fopenmp -Wl,-z,execstack --offload-arch=gfx942 -lflang_rt.hostdevice -fPIC -fconvert=big-endian -Qunused-arguments -fopenmp -Wl,-z,execstack --offload-arch=gfx942 -O1 -g -c yomvirt1.F90 -o yomvirt1.F90.o
+FC="amdflang -Wl,--export-dynamic -fopenmp -Wl,-z,execstack --offload-arch=gfx942 -lflang_rt.hostdevice -fPIC -fconvert=big-endian -Qunused-arguments -fopenmp -Wl,-z,execstack --offload-arch=gfx942 -O1 -g"
 
-amdflang -Wl,--export-dynamic -fopenmp -Wl,-z,execstack --offload-arch=gfx942 -lflang_rt.hostdevice -fPIC -fconvert=big-endian -Qunused-arguments -fopenmp -Wl,-z,execstack --offload-arch=gfx942 -O1 -g -c example_open_existing.F90 -o example_open_existing.F90.o
-
+$FC -c yomvirt1.F90 -o yomvirt1.F90.o
+$FC -c example_open_existing.F90 -o example_open_existing.F90.o
 \rm -f libodb.a
-
 ar crv libodb.a yomvirt1.F90.o
-
-amdflang -Wl,--export-dynamic -fopenmp -Wl,-z,execstack --offload-arch=gfx942 -lflang_rt.hostdevice -fPIC -fconvert=big-endian -Qunused-arguments -fopenmp -Wl,-z,execstack --offload-arch=gfx942 -O1 -g example_open_existing.F90.o -o example_open_existing.x libodb.a libodb.a 
+$FC example_open_existing.F90.o -o example_open_existing.x libodb.a libodb.a 
 

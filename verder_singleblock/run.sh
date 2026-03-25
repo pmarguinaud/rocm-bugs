01f2d87df535dead86bca8758116268a8775cf6b
Philippe Marguinaud <philippe.marguinaud@meteo.fr>
2026-03-25 16:14:11 +0100

    Start using run.sh

--- verder_singleblock/run.sh
+++ verder_singleblock/run.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+set -x
+set -e
+
 amdflang -fPIC -fconvert=big-endian -Qunused-arguments -fopenmp -Wl,-z,execstack --offload-arch=gfx942 -lflang_rt.hostdevice -lm -O1 -g -c verder_singleblock.F90
 
